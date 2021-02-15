265becb57f85fecc9aab2ea01884e1fada560a15
Pranay S. Yadav <mail@pranaysy.com>
2021-02-15 10:59:25 +0530

    Completed script for deriving CCM-based NCA estimates
    Computes based on 3 models: ETCP, ETCE, LZP with top 10% channel pairs

--- code/proc_33_causal_combine_results.py
+++ code/proc_33_causal_combine_results.py
@@ -11,12 +11,9 @@ ccm_folder = "../results/causal/"
 model_folder = "../results/model_complexities/"
 
 # Initialize empty lists for aggregation
-dfs_etc_wide = []
-dfs_etc_long = []
-dfs_etc_tm = []
-dfs_lz_wide = []
-dfs_lz_long = []
-dfs_lz_tm = []
+dfs_etcp = []
+dfs_etce = []
+dfs_lzp = []
 
 for subject in df.subject_id:
 
@@ -24,84 +21,71 @@ for subject in df.subject_id:
     df_file_name = "%s/%s_causal.csv" % (ccm_folder, subject)
     df_ccm = pd.read_csv(df_file_name, index_col=0)
 
-    ## Complexity estimate: ETC
+    # get number of pairs to aggregate over for k=10% (32C2 = 496 pairs for 32 channels)
+    k = round(0.1 * len(df_ccm["channel_pair"].unique()))
 
-    # extract for channel x
-    etc_x = df_ccm.groupby(["channel_x", "epoch"])["ETC_x"].min().reset_index()
-    etc_x = etc_x.rename(columns={"channel_x": "channel", "ETC_x": "ETC"})
+    ## NCA estimate: ETCP
 
-    # extract for channel y
-    etc_y = df_ccm.groupby(["channel_y", "epoch"])["ETC_y"].min().reset_index()
-    etc_y = etc_y.rename(columns={"channel_y": "channel", "ETC_y": "ETC"})
-
-    # combine (take union) of the above two
-    etc_long = (
-        pd.concat([etc_x, etc_y])  # pd.merge can be used with "outer" (union) join
-        .groupby(["channel", "epoch"])["ETC"]
-        .min()
+    etcp = (
+        df_ccm.melt(id_vars="epoch", value_vars=["ETCP_x_to_y", "ETCP_y_to_x"])
+        .groupby(["epoch"])["value"]
+        .nlargest(k)  # select k largest pairs
+        .groupby("epoch")
+        .apply(lambda x: trim_mean(x, 0.1))  # 10% trimmed means across top k pairs
+        .to_frame()
         .reset_index()
     )
 
-    # convert to wide form with one column per channel
-    etc_wide = etc_long.pivot(index="epoch", columns="channel", values="ETC")
+    ## NCA estimate: ETCE
 
-    # compute 20% trimmed means across epochs for per channel aggregation
-    etc_tm = etc_long.groupby("channel")["ETC"].apply(lambda x: trim_mean(x, 0.2))
-
-    ## Complexity estimate: LZ76
-
-    # extract for channel x
-    lz_x = df_ccm.groupby(["channel_x", "epoch"])["LZ_x"].min().reset_index()
-    lz_x = lz_x.rename(columns={"channel_x": "channel", "LZ_x": "LZ"})
-
-    # extract for channel y
-    lz_y = df_ccm.groupby(["channel_y", "epoch"])["LZ_y"].min().reset_index()
-    lz_y = lz_y.rename(columns={"channel_y": "channel", "LZ_y": "LZ"})
-
-    # combine (take union) of the above two
-    lz_long = (
-        pd.concat([lz_x, lz_y])  # pd.merge can be used with "outer" (union) join
-        .groupby(["channel", "epoch"])["LZ"]
-        .min()
+    etce = (
+        df_ccm.melt(id_vars="epoch", value_vars=["ETCE_x_to_y", "ETCE_y_to_x"])
+        .groupby(["epoch"])["value"]
+        .nlargest(k)  # select k largest pairs
+        .groupby("epoch")
+        .apply(lambda x: trim_mean(x, 0.1))  # 10% trimmed means across top k pairs
+        .to_frame()
         .reset_index()
     )
 
-    # convert to wide form with one column per channel
-    lz_wide = lz_long.pivot(index="epoch", columns="channel", values="LZ")
+    ## NCA estimate: LZP
 
-    # compute 20% trimmed means across epochs for per channel aggregation
-    lz_tm = lz_long.groupby("channel")["LZ"].apply(lambda x: trim_mean(x, 0.2))
+    lzp = (
+        df_ccm.melt(id_vars="epoch", value_vars=["LZP_x_to_y", "LZP_y_to_x"])
+        .groupby(["epoch"])["value"]
+        .nlargest(k)  # select k largest pairs
+        .groupby("epoch")
+        .apply(lambda x: trim_mean(x, 0.1))  # 10% trimmed means across top k pairs
+        .to_frame()
+        .reset_index()
+    )
 
     ## Clean up and aggregate
     # add subject identifiers
-    etc_long["subject_id"] = subject
-    etc_wide["subject_id"] = subject
-    etc_tm["subject_id"] = subject
-    lz_long["subject_id"] = subject
-    lz_wide["subject_id"] = subject
-    lz_tm["subject_id"] = subject
+    etcp["subject_id"] = subject
+    etce["subject_id"] = subject
+    lzp["subject_id"] = subject
 
     # accumulate estimates
-    dfs_etc_long.append(etc_long)
-    dfs_etc_wide.append(etc_wide)
-    dfs_etc_tm.append(etc_tm)
-    dfs_lz_long.append(lz_long)
-    dfs_lz_wide.append(lz_wide)
-    dfs_lz_tm.append(lz_tm)
+    dfs_etcp.append(etcp)
+    dfs_etce.append(etce)
+    dfs_lzp.append(lzp)
 
 # combine all dataframes, add ages and save
 for label, dfx in zip(
-    ["etc_long", "etc_wide", "lz_long", "lz_wide"],
-    [dfs_etc_long, dfs_etc_wide, dfs_lz_long, dfs_lz_wide],
+    ["nca_etce", "nca_etcp", "nca_lzp"], [dfs_etce, dfs_etcp, dfs_lzp]
 ):
 
-    dfo = pd.concat(dfx, axis=0, sort=False)
-    dfo = dfo.merge(df, on="subject_id")
-    dfo.to_csv(f"%s/complexity_{label}.csv" % ccm_folder, index=False)
+    # Concatenate and compute means across epochs
+    dfo = pd.concat(dfx)
+    dfo_mean = dfo.groupby("subject_id")["value"].mean().reset_index()
 
-for label, dfx in zip(["etc_tm", "lz_tm"], [dfs_etc_tm, dfs_lz_tm]):
-
-    dfo = pd.concat(dfx, axis=1, sort=False).T
+    # Add ages
     dfo = dfo.merge(df, on="subject_id")
+    dfo_mean = dfo_mean.merge(df, on="subject_id")
+
     dfo.to_csv(f"%s/complexity_{label}.csv" % ccm_folder, index=False)
     dfo.to_csv(f"%s/complexity_{label}.csv" % model_folder, index=False)
+
+    dfo_mean.to_csv(f"%s/complexity_{label}_mean.csv" % ccm_folder, index=False)
+    dfo_mean.to_csv(f"%s/complexity_{label}_mean.csv" % model_folder, index=False)

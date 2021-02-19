4643126b1f2efc9f2405b110dc2bffdf8bca2e76
Pranay S. Yadav <mail@pranaysy.com>
2021-02-19 09:58:30 +0530

    Updated comment steps

--- code/matlab_scripts/tw_part_1.m
+++ code/matlab_scripts/tw_part_1.m
@@ -24,30 +24,43 @@
 %
 % --------------------------------------------------------------------------------------
 
-% Step 0: Initialize path to .BDF data file
+% Step 0: Clear environment and initialize path to .BDF data file
 
 
 % Step 1: Launch EEGLAB
 
 
 % Step 2: Read .BDF file using the BIOSIG interface - only the first 32 channels
+%   add subject identifier as file name
 
 
-% Step 3: Fix channel labels
+% Step 3: Update dataset comments with datetime stamp and relevant metadata
+
+
+% Step 4: Fix channel labels
 chan_names = split({EEG.chanlocs.labels}, "-"); % Split string based on hyphen
 chan_names = squeeze(chan_names(:, :, 2)); % Take the right segments of the split
 [EEG.chanlocs.labels] = chan_names{:}; % Assign to struct field
 
-% Step 4: Add channel locations using default BESA 10-5 location file
+% Step 5: Update dataset comments decsribing step 4
 
 
-% Step 5: Make a copy of channel locations and store separately inside EEG struct
+% Step 6: Add channel locations using default BESA 10-5 location file
+
+
+% Step 7: Update dataset comments decsribing step 6
+
+
+% Step 8: Make a copy of channel locations and store separately inside EEG struct
 %   This will be useful later on for interpolation
 
 
-% Step 6: Save data as EEGLAB .set file
+% Step 9: Update dataset comments decsribing step 8
 
 
-% Step 7: Redraw EEGLAB GUI
+% Step 10: Save data as EEGLAB .set file
+
+
+% Step 11: Redraw EEGLAB GUI
 
 
